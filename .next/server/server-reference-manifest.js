d3bb42901acf0d633938319b8c2ffd13cdce8322
wojtek <bartzdev@icloud.com>
2024-07-31 19:05:44 +1000

    refactor

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wUmQOSjWYHxqeb8ekFv9FA2G0HXiC/yeAHbLjEU46zE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AzlSWktfzEzWOzwIW7eXfntemVi+OX0zcPeWQ3DgFGU=\"\n}"
